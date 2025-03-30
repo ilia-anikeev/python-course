611950e6a1ae16594f543254c9fd60a1412372d6
Ilia Anikeev <ilia.anikeev@gmail.com>
2025-03-30 23:46:17 +0300

    fix

--- hw3/sol/matrix_lib.py
+++ hw3/sol/matrix_lib.py
@@ -65,18 +65,3 @@ class Matrix(HashableMatrixMixin):
     def save_to_file(self, filename):
         with open(filename, 'w') as file:
             file.write(str(self))
-
-
-# Пример использования
-np.random.seed(0)
-matrix_a = Matrix(np.random.randint(0, 10, (10, 10)).tolist())
-matrix_b = Matrix(np.random.randint(0, 10, (10, 10)).tolist())
-
-result_add = matrix_a + matrix_b
-result_add.save_to_file('matrix_add.txt')
-
-result_mul = matrix_a * matrix_b
-result_mul.save_to_file('matrix_mul.txt')
-
-result_matmul = matrix_a @ matrix_b
-result_matmul.save_to_file('matrix_matmul.txt')
